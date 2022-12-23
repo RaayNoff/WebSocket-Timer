3a5fc8f7fe82c9897b095b002d8e0b1fb805de5b
RaayNoff <kraxtv471365@gmail.com>
2022-12-23 13:31:58 +0700

    N: Added base ws code

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const cors = require("cors");
+const app = express();
+const expressWs = require("express-ws")(app);
+
+app.ws("/", (ws, req) => {
+	ws.on("message", (msg) => {
+		try {
+			const msgBody = JSON.parse(msg);
+			console.log(msgBody);
+		} catch (error) {
+			console.error(
+				'======== Cannot handle ws.on("message") event ========\n',
+				error,
+			);
+		}
+	});
+});
 
 const PORT = process.env.PORT || 8000;
-
-const app = express();
-app.use(cors({}));
-app.use(express.json());
 
 app.listen(PORT, () => {
 	console.log(`SERVER STARTED ON PORT: ${PORT}`);
