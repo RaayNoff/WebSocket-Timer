ac9a07380046902a935ab49e4f669dc25d10dcdc
RaayNoff <kraxtv471365@gmail.com>
2022-12-23 12:57:28 +0700

    N: Added base of homepage

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,5 +1,34 @@
-const Home = () => {
-	return <div>Home</div>;
+import { FC, useState } from "react";
+import { Button, Input } from "shared";
+
+import s from "./style.module.scss";
+
+const Home: FC = () => {
+	const [inputValue, setInputValue] = useState("");
+
+	const startBiddingHandler = () => {
+		console.log("Started bidding");
+	};
+
+	return (
+		<main className={s.home}>
+			<div className="container">
+				<div className={s.home__container}>
+					<h1 className={s.home__title}>Welcome to trading platform</h1>
+					<Input
+						value={inputValue}
+						setValue={setInputValue}
+						placeholder="Bidding ID"
+						className={s.home__input}
+					/>
+					<section className={s.home__buttons}>
+						<Button callback={startBiddingHandler}>Start bidding</Button>
+						<Button>Enter bidding</Button>
+					</section>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default Home;
